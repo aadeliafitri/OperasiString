f88f767ddbdb458bb5227a78bdb4f82f7227615d
WINDOWS 10 <WINDOWS 10@DESKTOP-EO5EC5V>
2018-08-14 21:29:31 +0700

    Coba method format

--- src/jobsheet11/Latihan1.java
+++ src/jobsheet11/Latihan1.java
@@ -68,5 +68,10 @@ public class Latihan1 {
         System.out.println("pecah berdasarkan huruf 'r' di variable tclean : ");
         for(int i=0;i<arrTr.length;i++)
             System.out.println(arrTr[i]);
+        
+        double d = 1545454000;
+        System.out.println(String.format("Uang saya %,.4f", d));
+        
+        System.out.println(String.format(Locale.forLanguageTag("in-ID"),"Uang saya %,.4f", d));
     }
 }
